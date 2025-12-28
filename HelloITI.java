a8a0eabac85fd20a333a8510dead249eab624e69
ahmadmofathi <ahmad.f2982016@gmail.com>
2025-12-28 12:45:25 +0200

    Class name is Hello ITI

--- HelloITI.java
+++ HelloITI.java
@@ -1,12 +1,12 @@
-class Hello2 {
-    public static void HelloITI() {
+class HelloITI {
+    public static void printHelloITI() {
         System.out.println("Hello, ITI!");
     }
     public static void HelloGit() {
         System.out.println("Hello, Git!");
     }
     public static void main(String[] args) {
-        HelloITI();
+        printHelloITI();
         HelloGit();
     }
 }
