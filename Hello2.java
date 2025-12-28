8d895a9809268ca838865a444f78a25a5fa10fbd
ahmadmofathi <ahmad.f2982016@gmail.com>
2025-12-28 12:33:37 +0200

    Added HelloGit to Hello2

--- Hello2.java
+++ Hello2.java
@@ -2,7 +2,11 @@ class Hello2 {
     public static void HelloITI() {
         System.out.println("Hello, ITI!");
     }
+    public static void HelloGit() {
+        System.out.println("Hello, Git!");
+    }
     public static void main(String[] args) {
         HelloITI();
+        HelloGit();
     }
 }
