add0e2186d7efdf93f2590ef89c73d70cb172263
ahmadmofathi <ahmad.f2982016@gmail.com>
2025-12-28 12:25:47 +0200

    Print Hello World

---
+++ Hello1.java
@@ -0,0 +1,8 @@
+class Hello1 {
+    public static void HelloWorld() {
+        System.out.println("Hello, World!");
+    }
+    public static void main(String[] args) {
+        HelloWorld();
+    }
+}
